7f3a7517a8c47299c4e7d2a3413b39975869be69
Adam Powell <adamm2powell@gmail.com>
2018-12-18 14:30:42 -0500

    Pass context correctly to the draft method

--- test/test.py
+++ test/test.py
@@ -13,6 +13,9 @@ class Test:
     def __init__(self, bot):
         self.bot = bot
         self.check_configs()
+        
+
+    async def on_ready(self):
         global transactionLog
         try:
             with open('transactionLog.json') as f:
@@ -34,16 +37,17 @@ class Test:
 
         await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
 
-    @commands.command()
-    async def draft(self, user : discord.Member, teamRole : discord.Role):
+    @commands.command(pass_context=True)
+    async def draft(self, ctx, user : discord.Member, teamRole : discord.Role):
         await self.bot.add_roles(user, teamRole)
+        server = ctx.message.server
         channel = transactionLog[server.id]
         await self.bot.say(server.get_channel(channel), user.mention + " was drafted onto the " + teamRole)
 
     @commands.command(pass_context=True)
-    async def setTransactionLogChannel(self,ctx,mlog:discord.Channel):
-        """Sets mod-log channel"""
-        transactionLog[ctx.message.server.id] = mlog.id
+    async def setTransactionLogChannel(self, ctx, tlog:discord.Channel):
+        """Sets transaction-log channel"""
+        transactionLog[ctx.message.server.id] = tlog.id
         await self.bot.say("Transaction Log channel set")
         with open('transactionLog.json', 'w+') as f:
             json.dump(transactionLog, f)
