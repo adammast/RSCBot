602de3a7c928ba1df0ed8fec1b75ab70ba2fb2d6
Adam Powell <adamm2powell@gmail.com>
2018-12-20 11:43:25 -0500

    Place nickname change between remove and add roles in draft

--- draft/draft.py
+++ draft/draft.py
@@ -53,14 +53,14 @@ class Draft:
                     leagueRole = self.find_role(server.roles, leagueRoleId)
                     channel = server.get_channel(channelId)
                     freeAgentRole = self.find_role(server.roles, server_dict['Free Agent'])
-                    if freeAgentRole in user.roles:
-                        await self.bot.remove_roles(user, freeAgentRole)
                     if teamRole in user.roles:
                         message = "{0} was kept by the {1}".format(user.mention, teamRole.mention)
                     else:
                         message = "{0} was drafted by the {1}".format(user.mention, teamRole.mention)
-                    await self.bot.add_roles(user, teamRole, leagueRole, franchiseRole)
+                    if freeAgentRole in user.roles:
+                        await self.bot.remove_roles(user, freeAgentRole)
                     await self.bot.change_nickname(user, "{0} | {1}".format(prefix, user.name))
+                    await self.bot.add_roles(user, teamRole, leagueRole, franchiseRole)
                     await self.bot.send_message(channel, message)
                 except LookupError:
                     await self.bot.say(":x: Could not find league role with id of {0}".format(leagueRoleId))
