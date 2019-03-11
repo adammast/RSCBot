f643bed79525316d733946147136e83a7e750824
Adam Powell <adamm2powell@gmail.com>
2019-03-11 10:50:27 -0400

    Add comand "giveRoleToAllWithRole"

--- bulkRoleManager/bulkRoleManager.py
+++ bulkRoleManager/bulkRoleManager.py
@@ -91,6 +91,30 @@ class BulkRoleManager:
             for user in notFound:
                 await self.bot.say(user)
 
+    @commands.command(pass_context=True, no_pm=True)
+    async def giveRoleToAllWithRole(self, ctx, currentRole : discord.Role, roleToGive : discord.Role):
+        """Gives the roleToGive to every member who already has the currentRole"""
+        count = 0
+        hadRoleCount = 0
+        countGiven = 0
+        for member in ctx.message.server.members:
+            if currentRole in member.roles:
+                count += 1
+                if roleToGive in member.roles:
+                    hadRoleCount += 1
+                else:
+                    await self.bot.add_roles(member, roleToGive)
+                    countGiven += 1
+        if count == 0:
+            message = ":x: Nobody has the {0} role".format(currentRole.name)
+        else:
+            message = ":white_check_mark: {0} user(s) had the {1} role".format(count, currentRole.name)
+            if hadRoleCount > 0:
+                message += ". {0} user(s) already had the {1} role".format(hadRoleCount, roleToGive.name)
+            if countGiven > 0:
+                message += ". {0} user(s) had the {1} role added to them".format(countGiven, roleToGive.name)
+        await self.bot.say(message)
+
     def get_player_nickname(self, user : discord.Member):
         if user.nick is not None:
             array = user.nick.split(' | ', 1)
