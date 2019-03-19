37cd32313d627d6121499179abd5b14986fb1cd4
Adam Powell <adamm2powell@gmail.com>
2019-03-19 09:46:04 -0400

    Add reading and writing commands

--- sheetTest/sheetTest.py
+++ sheetTest/sheetTest.py
@@ -1,5 +1,6 @@
 import discord
 import gspread
+import typing
 
 from discord.ext import commands
 from oauth2client.service_account import ServiceAccountCredentials
@@ -22,6 +23,28 @@ class SheetTest:
 
         wks.append_row(['This should go in column 1', 'This should go in column 2'])
         await self.bot.say(wks.get_all_records())
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def read(self, ctx, rowIndex: typing.Optional[int] = -1, columnIndex: typing.Optional[int] = -1):
+        """Reads data from the spreadsheet at the specified row and column, or reads all data if no row or column are specified."""
+        wks = self.gc.open('Test').sheet1
+        if(rowIndex == -1):
+            await self.bot.say(wks.get_all_records())
+        elif(columnIndex == -1):
+            await self.bot.say(wks.get_all_records())
+        else:
+            await self.bot.say(wks.cell(rowIndex, columnIndex).value)
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def write(self, ctx, message, rowIndex: typing.Optional[int] = -1, columnIndex: typing.Optional[int] = -1):
+        """Writes data to the spreadsheet at the specified row and column, or at the bottom of the sheet if no row or column are specified."""
+        wks = self.gc.open('Test').sheet1
+        if(rowIndex == -1):
+            wks.append_row(message)
+        elif(columnIndex == -1):
+            wks.append_row(message)
+        else:
+            wks.update_cell(rowIndex, columnIndex, message)
         
        
 
