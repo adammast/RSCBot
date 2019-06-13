c2e3a2a41b889af9869f5c7d94b5e99c8526819b
Adam Powell <adamm2powell@gmail.com>
2019-06-13 12:53:25 -0400

    Add IR label for roster members

--- teamManager/teamManager.py
+++ teamManager/teamManager.py
@@ -18,6 +18,7 @@ class TeamManager:
     TIER_ROLE_KEY = "Tier Role"
     GM_ROLE = "General Manager"
     CAPTAN_ROLE = "Captain"
+    IR_ROLE = "IR"
     PERM_FA_ROLE = "PermFA"
 
     def __init__(self, bot):
@@ -248,6 +249,12 @@ class TeamManager:
                 return True
         return False
 
+    def on_IR(self, member):
+        for role in member.roles:
+            if role.name == self.IR_ROLE:
+                return True
+        return False
+
     def teams_for_user(self, ctx, user):
         tiers = self._tiers(ctx)
         teams = []
@@ -295,6 +302,8 @@ class TeamManager:
         name = member.nick if member.nick else member.name
         if self.is_captain(member):
             extraRoles.append("C")
+        if self.on_IR(member):
+            extraRoles.append("IR")
         roleString = ""
         if extraRoles:
             roleString = " ({0})".format("|".join(extraRoles))
