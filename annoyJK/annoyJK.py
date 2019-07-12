2d714e350fcca9b10e348aef8b0477dab0347cfb
Adam Powell <adamm2powell@gmail.com>
2019-07-12 18:12:18 -0400

    Add cog to react on message to a specific user

---
+++ annoyJK/annoyJK.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+class AnnoyJK:
+
+    JK_ID = 226869393626234881
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        if message.author.id == self.JK_ID:
+            await self.bot.add_reaction(message, '👍')
+
+        await self.bot.process_commands(message)
+
+def setup(bot):
+    bot.add_cog(AnnoyJK(bot))
