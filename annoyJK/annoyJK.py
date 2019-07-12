342aec1f4adc6a84c9a8c8b78fbd71dddeb09d9b
Adam Powell <adamm2powell@gmail.com>
2019-07-12 18:31:24 -0400

    Have bot output message author id

--- annoyJK/annoyJK.py
+++ annoyJK/annoyJK.py
@@ -8,6 +8,7 @@ class AnnoyJK:
         self.bot = bot
 
     async def on_message(self, message):
+        await self.bot.say(message.author.id)
         if message.author.id == self.JK_ID:
             await self.bot.add_reaction(message, '👍')
 
