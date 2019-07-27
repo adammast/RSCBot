336a67a4fc01ac01a2c7b6fbb1c575fbaffc2c9f
adammast <adamm2powell@gmail.com>
2019-07-27 18:29:35 -0400

    Add async and await statements where needed

--- prefixManager/prefixManager.py
+++ prefixManager/prefixManager.py
@@ -58,7 +58,7 @@ class PrefixManager(commands.Cog):
     @commands.guild_only()
     async def getPrefixes(self, ctx):
         """Used to get all prefixes in the prefix dictionary"""
-        prefixes = self._prefixes(ctx)
+        prefixes = await self._prefixes(ctx)
 
         if(len(prefixes.items()) > 0):
             message = "```Prefixes:"
@@ -73,31 +73,31 @@ class PrefixManager(commands.Cog):
     @checks.admin_or_permissions(manage_guild=True)
     async def removePrefix(self, ctx, gm_name: str):
         """Used to remove a single prefix"""
-        prefixes = self._prefixes(ctx)
+        prefixes = await self._prefixes(ctx)
         try:
             del prefixes[gm_name]
         except ValueError:
             await ctx.send("{0} does not have a prefix.".format(gm_name))
             return
-        self._save_prefixes(ctx, prefixes)
+        await self._save_prefixes(ctx, prefixes)
         await ctx.send("Done.")
 
     @commands.guild_only()
     @checks.admin_or_permissions(manage_guild=True)
     async def clearPrefixes(self, ctx):
         """Used to clear the prefix dictionary"""
-        prefixes = self._prefixes(ctx)
+        prefixes = await self._prefixes(ctx)
 
         try:
             prefixes.clear()
-            self._save_prefixes(ctx, prefixes)
+            await self._save_prefixes(ctx, prefixes)
             await ctx.send(":white_check_mark: All prefixes have been removed from dictionary")
         except:
             await ctx.send(":x: Something went wrong when trying to clear the prefix dictionary")
 
     @commands.guild_only()
     async def lookupPrefix(self, ctx, gmName: str):
-        prefixes = self._prefixes(ctx)
+        prefixes = await self._prefixes(ctx)
 
         try:
             prefix = prefixes[gmName]
@@ -114,7 +114,7 @@ class PrefixManager(commands.Cog):
         raise LookupError('No role with id: {0} found in server roles'.format(role_id))
 
     async def _add_prefix(self, ctx, gm_name: str, prefix: str):
-        prefixes = self._prefixes(ctx)
+        prefixes = await self._prefixes(ctx)
 
         proper_gm_name = self._get_proper_gm_name(ctx, gm_name)
 
@@ -135,7 +135,7 @@ class PrefixManager(commands.Cog):
             prefixes[proper_gm_name] = prefix
         except:
             return False
-        self._save_prefixes(ctx, prefixes)
+        await self._save_prefixes(ctx, prefixes)
         return True
 
     def _get_proper_gm_name(self, ctx, gm_name):
@@ -149,14 +149,11 @@ class PrefixManager(commands.Cog):
             except:
                 continue
 
-    def _all_data(self, ctx):
-        return self.data_cog.load(ctx, self.DATASET)
-
-    def _prefixes(self, ctx):
+    async def _prefixes(self, ctx):
         prefixes = await self.config.guild(ctx.guild).Prefixes()
         return prefixes
 
-    def _save_prefixes(self, ctx, prefixes):
+    async def _save_prefixes(self, ctx, prefixes):
         await self.config.guild(ctx.guild).Prefixes.set(prefixes)
 
 def setup(bot):
