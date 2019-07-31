716452e3c27ddbeabb3ccb642754fcfc59f5a05f
Adam Powell <adamm2powell@gmail.com>
2019-07-31 11:21:20 -0400

    Add sheet test cog and link it to Tracker test sheet

---
+++ sheetTest/__init__.py
@@ -0,0 +1,4 @@
+from .sheetTest import SheetTest
+
+def setup(bot):
+    bot.add_cog(SheetTest())
--- sheetTest/sheetTest.py
+++ sheetTest/sheetTest.py
@@ -2,21 +2,21 @@ import discord
 import gspread
 import typing
 
-from discord.ext import commands
+from redbot.core import commands
+from redbot.core import checks
 from oauth2client.service_account import ServiceAccountCredentials
 
-class SheetTest:
+class SheetTest(commands.Cog):
     """Test cog for accessing and editing a Google Sheet"""
 
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('data/sheetTest/Python Test-21ff839e46f8.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('trackers-248415-645b0fa3512d.json', scope)
     gc = gspread.authorize(credentials)
 
-    def __init__(self, bot):
-        self.bot = bot
-
     @commands.guild_only()
-    async def test(self, ctx):
+    @commands.command()
+    @checks.is_owner()
+    async def testWrite(self, ctx):
         """Edits the spreadsheet"""
         wks = self.gc.open('Test').sheet1
         await ctx.send(wks.get_all_records())
@@ -25,6 +25,8 @@ class SheetTest:
         await ctx.send(wks.get_all_records())
 
     @commands.guild_only()
+    @commands.command()
+    @checks.is_owner()
     async def read(self, ctx, rowIndex: int = None, columnIndex: int = None):
         """Reads data from the spreadsheet at the specified row and column, or reads all data if no row or column are specified."""
         wks = self.gc.open('Test').sheet1
@@ -34,6 +36,8 @@ class SheetTest:
             await ctx.send(wks.get_all_records())
 
     @commands.guild_only()
+    @commands.command()
+    @checks.is_owner()
     async def write(self, ctx, message, rowIndex: int = None, columnIndex: int = None):
         """Writes data to the spreadsheet at the specified row and column, or at the bottom of the sheet if no row or column are specified."""
         wks = self.gc.open('Test').sheet1
@@ -41,8 +45,3 @@ class SheetTest:
             wks.update_cell(rowIndex, columnIndex, message)
         else:
             wks.append_row([message])
-        
-       
-
-def setup(bot):
-    bot.add_cog(SheetTest(bot))
