fd6cf4e2d034446c20532db2f60cf7913800b5cd
Adam Powell <adamm2powell@gmail.com>
2019-07-31 16:40:21 -0400

    Make test command to check where file is created

--- sheetTest/sheetTest.py
+++ sheetTest/sheetTest.py
@@ -1,6 +1,8 @@
 import discord
 import gspread
-import typing
+import requests
+import csv
+import datetime
 
 from redbot.core import commands
 from redbot.core import checks
@@ -10,38 +12,37 @@ class SheetTest(commands.Cog):
     """Test cog for accessing and editing a Google Sheet"""
 
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('trackers-248415-645b0fa3512d.json', scope)
-    gc = gspread.authorize(credentials)
+    #credentials = ServiceAccountCredentials.from_json_keyfile_name('trackers-248415-645b0fa3512d.json', scope)
+    #gc = gspread.authorize(credentials)
+
+    now = datetime.datetime.now()
+    readibletime =  now.strftime("%Y-%m-%d_%H-%M-%S")
+    Outputcsv = "Scrapes/%s.csv" % (readibletime)
 
     @commands.guild_only()
     @commands.command()
     @checks.is_owner()
     async def testWrite(self, ctx):
-        """Edits the spreadsheet"""
-        wks = self.gc.open('Test').sheet1
-        await ctx.send(wks.get_all_records())
+        open(Outputcsv, 'w', newline='')
 
-        wks.append_row(['This should go in column 1', 'This should go in column 2'])
-        await ctx.send(wks.get_all_records())
+    # @commands.guild_only()
+    # @commands.command()
+    # @checks.is_owner()
+    # async def read(self, ctx, rowIndex: int = None, columnIndex: int = None):
+    #     """Reads data from the spreadsheet at the specified row and column, or reads all data if no row or column are specified."""
+    #     wks = self.gc.open('Public Tracker List (Test)').sheet1
+    #     if(rowIndex and columnIndex):
+    #         await ctx.send(wks.cell(rowIndex, columnIndex).value)
+    #     else:
+    #         await ctx.send(wks.get_all_records())
 
-    @commands.guild_only()
-    @commands.command()
-    @checks.is_owner()
-    async def read(self, ctx, rowIndex: int = None, columnIndex: int = None):
-        """Reads data from the spreadsheet at the specified row and column, or reads all data if no row or column are specified."""
-        wks = self.gc.open('Public Tracker List (Test)').sheet1
-        if(rowIndex and columnIndex):
-            await ctx.send(wks.cell(rowIndex, columnIndex).value)
-        else:
-            await ctx.send(wks.get_all_records())
-
-    @commands.guild_only()
-    @commands.command()
-    @checks.is_owner()
-    async def write(self, ctx, message, rowIndex: int = None, columnIndex: int = None):
-        """Writes data to the spreadsheet at the specified row and column, or at the bottom of the sheet if no row or column are specified."""
-        wks = self.gc.open('Test').sheet1
-        if(rowIndex and columnIndex):
-            wks.update_cell(rowIndex, columnIndex, message)
-        else:
-            wks.append_row([message])
+    # @commands.guild_only()
+    # @commands.command()
+    # @checks.is_owner()
+    # async def write(self, ctx, message, rowIndex: int = None, columnIndex: int = None):
+    #     """Writes data to the spreadsheet at the specified row and column, or at the bottom of the sheet if no row or column are specified."""
+    #     wks = self.gc.open('Test').sheet1
+    #     if(rowIndex and columnIndex):
+    #         wks.update_cell(rowIndex, columnIndex, message)
+    #     else:
+    #         wks.append_row([message])
