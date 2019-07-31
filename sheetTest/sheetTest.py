8f0a6312a1722343b178f67e4820e12600399b3e
Adam Powell <adamm2powell@gmail.com>
2019-07-31 11:34:15 -0400

    Add json file to the bot explicitely

--- sheetTest/sheetTest.py
+++ sheetTest/sheetTest.py
@@ -10,7 +10,7 @@ class SheetTest(commands.Cog):
     """Test cog for accessing and editing a Google Sheet"""
 
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('../../data/sheetTest/trackers-248415-645b0fa3512d.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('trackers-248415-645b0fa3512d.json', scope)
     gc = gspread.authorize(credentials)
 
     @commands.guild_only()
@@ -29,7 +29,7 @@ class SheetTest(commands.Cog):
     @checks.is_owner()
     async def read(self, ctx, rowIndex: int = None, columnIndex: int = None):
         """Reads data from the spreadsheet at the specified row and column, or reads all data if no row or column are specified."""
-        wks = self.gc.open('Test').sheet1
+        wks = self.gc.open('Public Tracker List (Test)').sheet1
         if(rowIndex and columnIndex):
             await ctx.send(wks.cell(rowIndex, columnIndex).value)
         else:
