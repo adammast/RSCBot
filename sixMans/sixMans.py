bce5a0eef8a5de90140f27a8c3d614d2c366e189
adammast <adamm2powell@gmail.com>
2019-08-03 16:57:13 -0400

    Alter inline setting for embed fields

--- sixMans/sixMans.py
+++ sixMans/sixMans.py
@@ -230,9 +230,9 @@ class SixMans(commands.Cog):
 
     def _format_queue_info(self, ctx, queue):
         embed = discord.Embed(title="{0} Info".format(queue.name), color=discord.Colour.blue())
-        embed.add_field(name="Channels", value="{}\n".format(", ".join([channel.mention for channel in queue.channels])), inline=False)
+        embed.add_field(name="Channels", value="{}".format(", ".join([channel.mention for channel in queue.channels])), inline=False)
         embed.add_field(name="Games Played", value="{}".format(queue.gamesPlayed), inline=True)
-        embed.add_field(name="All-Time Unique Players", value="{}\n".format(len(queue.players)), inline=True)
+        embed.add_field(name="All-Time Unique Players", value="{}".format(len(queue.players)), inline=False)
         embed.add_field(name="Points Per Series Played", value="{}".format(queue.points[pp_play_key]), inline=True)
         embed.add_field(name="Points Per Series Won", value="{}".format(queue.points[pp_win_key]), inline=True)
         return embed
