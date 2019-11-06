9edd964fab5f9fe95d6879b152e126135562b097
Adam Powell <adamm2powell@gmail.com>
2019-11-06 11:07:19 -0500

    Check if match day is string before making it lowercase

--- match/match.py
+++ match/match.py
@@ -311,8 +311,10 @@ class Match(commands.Cog):
     async def _team_day_match_index(self, ctx, team, match_day):
         team_days_index = await self._team_days_index(ctx)
         team_days_index =  {k.lower(): v.lower() for k, v in team_days_index.items()}
+        if isinstance(match_day, str):
+            match_day = match_day.lower()
         return team_days_index.get(
-            self._team_day_key(team.lower(), match_day.lower()))
+            self._team_day_key(team.lower(), match_day))
 
     def _team_day_key(self, team, match_day):
         return "{0}|{1}".format(team, match_day)
