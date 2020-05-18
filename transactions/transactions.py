ff990ff5305cdd749d4c3b5a3764430ffc350cf0
Benjamin Hunt <bjh1381@rit.edu>
2020-05-17 22:24:54 -0400

    user's FA roles used in ?sub to remove and reassign tier roles as appropriate

--- transactions/transactions.py
+++ transactions/transactions.py
@@ -284,7 +284,7 @@ class Transactions(commands.Cog):
 
     async def get_tier_role_for_fa(self, ctx, user : discord.Member):
         # TODO: THIS FUN. use get_fa_roles functions
-        fa_roles = await self.get_free_agent_roles(ctx)
+        fa_roles = await self.find_user_free_agent_roles(ctx, user)
         fa_roles.remove(self.team_manager_cog._find_role_by_name(ctx, "Free Agent"))
         tier_role_name = fa_roles[0].name[:-2]
         tier_role = self.team_manager_cog._find_role_by_name(ctx, tier_role_name)
