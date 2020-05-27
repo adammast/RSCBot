4b871de376e4cb150a6af283d30d7bf5f007d65f
Benjamin Hunt <bjh1381@rit.edu>
2020-05-27 18:03:21 -0400

    prefixes edited with predefined function in team_manager cog

--- transactions/transactions.py
+++ transactions/transactions.py
@@ -103,7 +103,7 @@ class Transactions(commands.Cog):
                     role_name = "{0}FA".format((await self.team_manager_cog.get_current_tier_role(ctx, user)).name)
                     tier_fa_role = self.team_manager_cog._find_role_by_name(ctx, role_name)
                 fa_role = self.team_manager_cog._find_role_by_name(ctx, "Free Agent")
-                await user.edit(nick="FA | {0}".format(self.get_player_nickname(user)))
+                await team_manager_cog._set_user_nickname_prefix(ctx, "FA", user)
                 await user.add_roles(tier_fa_role, fa_role)
                 gm_name = self._get_gm_name(ctx, franchise_role)
                 message = "{0} was cut by the {1} ({2} - {3})".format(user.mention, team_name, gm_name, tier_role.name)
@@ -240,8 +240,7 @@ class Transactions(commands.Cog):
                 currentTier = await self.team_manager_cog.get_current_tier_role(ctx, user)
                 if currentTier is not None and currentTier != tier_role:
                     await user.remove_roles(currentTier)
-                await user.edit(nick="{0} | {1}".format(prefix, self.get_player_nickname(user)))
-                await 
+                await team_manager_cog._set_user_nickname_prefix(ctx, prefix, user)
                 await user.add_roles(tier_role, leagueRole, franchise_role)
 
 
