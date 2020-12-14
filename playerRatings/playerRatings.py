a5ae69c7344429bb1d3b38ebebd6e28a950a6f26
adammast <adamm2powell@gmail.com>
2020-12-14 16:01:22 -0500

    Fix issue with the tier parameter in the player leaderboard

--- playerRatings/playerRatings.py
+++ playerRatings/playerRatings.py
@@ -145,7 +145,7 @@ class PlayerRatings(commands.Cog):
 
     @commands.guild_only()
     @commands.command(aliases=["plb"])
-    async def playerLeaderboard(self, ctx, tier: None):
+    async def playerLeaderboard(self, ctx, tier = None):
         """Shows the top ten players in terms of current Elo rating. If tier is specified it only looks at players in that tier."""
         await self.load_players(ctx)
         players = self.players
@@ -263,9 +263,9 @@ class PlayerRatings(commands.Cog):
     def embed_player_info(self, player):
         embed = discord.Embed(title="{0}".format(player.member.name), color=discord.Colour.blue())
         embed.set_thumbnail(url=player.member.avatar_url)
-        embed.add_field(name="Games Played", value="{}\n".format(player.wins + player.losses), inline=True)
-        embed.add_field(name="Record", value="{0} - {1}\n".format(player.wins, player.losses), inline=True)
-        embed.add_field(name="Elo Rating", value="{}\n".format(player.elo_rating), inline=True)
+        embed.add_field(name="Games Played", value="{}\n".format(player.wins + player.losses), inline=False)
+        embed.add_field(name="Record", value="{0} - {1}\n".format(player.wins, player.losses), inline=False)
+        embed.add_field(name="Elo Rating", value="{}\n".format(player.elo_rating), inline=False)
         return embed
 
     def embed_leaderboard(self, ctx, sorted_players):
@@ -281,14 +281,14 @@ class PlayerRatings(commands.Cog):
             if index > 10:
                 break
 
-        embed.add_field(name="Highest Elo Rating", value=message, inline=True)
+        embed.add_field(name="Highest Elo Rating", value=message, inline=False)
         return embed
 
     def embed_game_results(self, player_1, player_2, player_1_wins: int, player_2_wins: int, player_1_new_elo, player_2_new_elo):
         embed = discord.Embed(title="{0} vs. {1}".format(player_1.member.name, player_2.member.name), color=discord.Colour.blue())
-        embed.add_field(name="Result", value="**{0}** {1} - {2} **{3}**\n".format(player_1.member.name, player_1_wins, player_2_wins, player_2.member.name), inline=True)
+        embed.add_field(name="Result", value="**{0}** {1} - {2} **{3}**\n".format(player_1.member.name, player_1_wins, player_2_wins, player_2.member.name), inline=False)
         embed.add_field(name="Updated Elo Rating", value="**{0}** = {1} ({2})\n**{3}** = {4} ({5})\n".format(player_1.member.name, player_1_new_elo, player_1_new_elo - int(player_1.elo_rating),
-            player_2.member.name, player_2_new_elo, player_2_new_elo - int(player_2.elo_rating)), inline=True)
+            player_2.member.name, player_2_new_elo, player_2_new_elo - int(player_2.elo_rating)), inline=False)
         return embed
 
     #endregion
