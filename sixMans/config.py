04130dfdbd094663a4d752439f7bfbd539bb12e0
Benjamin Hunt <bjh1381@rit.edu>
2021-03-25 17:57:01 -0400

    includes config file that was not included in previous commit -m

---
+++ sixMans/config.py
@@ -0,0 +1,46 @@
+
+class config:
+    room_pass = [
+        'octane', 'takumi', 'dominus', 'hotshot', 'batmobile', 'mantis',
+        'paladin', 'twinmill', 'centio', 'breakout', 'animus', 'venom',
+        'xdevil', 'endo', 'masamune', 'merc', 'backfire', 'gizmo',
+        'roadhog', 'armadillo', 'hogsticker', 'luigi', 'mario', 'samus',
+        'sweettooth', 'cyclone', 'imperator', 'jager', 'mantis', 'nimbus',
+        'samurai', 'twinzer', 'werewolf', 'maverick', 'artemis', 'charger',
+        'skyline', 'aftershock', 'boneshaker', 'delorean', 'esper',
+        'fast4wd', 'gazella', 'grog', 'jeep', 'marauder', 'mclaren',
+        'mr11', 'proteus', 'ripper', 'scarab', 'tumbler', 'triton',
+        'vulcan', 'zippy',
+
+        'aquadome', 'beckwith', 'champions', 'dfh', 'mannfield',
+        'neotokyo', 'saltyshores', 'starbase', 'urban', 'utopia',
+        'wasteland', 'farmstead', 'arctagon', 'badlands', 'core707',
+        'dunkhouse', 'throwback', 'underpass', 'badlands',
+
+        '20xx', 'biomass', 'bubbly', 'chameleon', 'dissolver', 'heatwave',
+        'hexed', 'labyrinth', 'parallax', 'slipstream', 'spectre',
+        'stormwatch', 'tora', 'trigon', 'wetpaint',
+
+        'ara51', 'ballacarra', 'chrono', 'clockwork', 'cruxe',
+        'discotheque', 'draco', 'dynamo', 'equalizer', 'gernot', 'hikari',
+        'hypnotik', 'illuminata', 'infinium', 'kalos', 'lobo', 'looper',
+        'photon', 'pulsus', 'raijin', 'reactor', 'roulette', 'turbine',
+        'voltaic', 'wonderment', 'zomba',
+
+        'unranked', 'prospect', 'challenger', 'risingstar', 'allstar',
+        'superstar', 'champion', 'grandchamp', 'bronze', 'silver', 'gold',
+        'platinum', 'diamond',
+
+        'dropshot', 'hoops', 'soccar', 'rumble', 'snowday', 'solo',
+        'doubles', 'standard', 'chaos',
+
+        'armstrong', 'bandit', 'beast', 'boomer', 'buzz', 'cblock',
+        'casper', 'caveman', 'centice', 'chipper', 'cougar', 'dude',
+        'foamer', 'fury', 'gerwin', 'goose', 'heater', 'hollywood',
+        'hound', 'iceman', 'imp', 'jester', 'junker', 'khan', 'marley',
+        'maverick', 'merlin', 'middy', 'mountain', 'myrtle', 'outlaw',
+        'poncho', 'rainmaker', 'raja', 'rex', 'roundhouse', 'sabretooth',
+        'saltie', 'samara', 'scout', 'shepard', 'slider', 'squall',
+        'sticks', 'stinger', 'storm', 'sultan', 'sundown', 'swabbie',
+        'tex', 'tusk', 'viper', 'wolfman', 'yuri'
+    ]
