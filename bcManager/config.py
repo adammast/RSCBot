bddea0fa97691d3037eda1a739727244e4c3048a
Benjamin Hunt <bjh1381@rit.edu>
2021-05-11 15:47:52 -0400

    ?bcreport finds replays uploaded by any participant of the invoker's match, and adds to the appropriate ballchasing subgroup

---
+++ bcManager/config.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+# Discord ID: Steam ID -- maybe handle multiple accounts?
+
+# ###############################################################################
+
+class config:
+    auth_token = 'Ght7UfDSmDeA58GPc3A8QnRfmM93LX7Z6U9g7LG0'
+    top_level_group = 'test-group-cl0dozmu3o'
+    search_count = 10
+    visibility = 'public'
+    team_identification = 'by-player-clusters'                  # setting -- Alternative: 'by-distinct-players'
+    player_identification = 'by-id'                             # setting -- Alternative 'by-name'
+
+    # Ballchasing subgroups: 1Premier, 2Master, etc.
+    tier_rank = {
+        "Premier": 1,
+        "Master": 2,
+        "Elite": 3,
+        "Major": 4,
+        "Minor": 5,
+        "Challenger": 6,
+        "Prospect": 7,
+        "Contender": 8,
+        "Amateur": 9
+    }
